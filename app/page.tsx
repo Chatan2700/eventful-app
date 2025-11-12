d5cb33e1f87944e9a262d5851e16cb81038d78a5
Sebastian Rios <juanriosubeda@gmail.com>
2025-11-12 10:50:16 -0800

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from "@/components/ExploreBtn";
 import React from "react";
 
 const Page = () => {
@@ -9,6 +10,18 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackatons, Meetups, Conferences and Workshops, All in one place
       </p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
